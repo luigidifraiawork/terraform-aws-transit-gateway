08c1b0ca468edfe0453513243da8152a227d6c56
Luigi Di Fraia <luigidifraia+workrelated@gmail.com>
2022-05-11 22:16:15 +0100

    A few rationalizations

--- examples/custom-route-tables/main.tf
+++ examples/custom-route-tables/main.tf
@@ -6,12 +6,28 @@ locals {
   name   = "ex-tgw-${replace(basename(path.cwd), "_", "-")}"
   region = "us-east-1"
 
+  azs = slice(data.aws_availability_zones.available.names, 0, 3)
+
+  config = {
+    networking = {
+      cidr = "10.1.0.0/16"
+    }
+    dev = {
+      cidr = "10.2.0.0/16"
+    }
+    qa = {
+      cidr = "10.3.0.0/16"
+    }
+  }
+
   tags = {
     Example    = local.name
     GithubRepo = "terraform-aws-eks"
     GithubOrg  = "terraform-aws-transit-gateway"
   }
 }
+
+data "aws_availability_zones" "available" {}
 
 ################################################################################
 # Transit Gateway Module
@@ -21,7 +37,6 @@ module "tgw" {
   source = "../../"
 
   name            = local.name
-  description     = "My TGW shared with several other AWS accounts"
   amazon_side_asn = 64532
 
   transit_gateway_cidr_blocks = ["10.99.0.0/24"]
@@ -44,82 +59,82 @@ module "tgw" {
 # Supporting resources
 ################################################################################
 
-module "vpc_shrd" {
+module "vpc_networking" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "~> 3.0"
 
-  name = "shrd-vpc"
-  cidr = "10.3.0.0/16"
+  name = "${local.name}-networking"
+  cidr = local.config.networking.cidr
 
-  azs             = ["${local.region}a", "${local.region}b", "${local.region}c"]
-  private_subnets = ["10.3.11.0/24", "10.3.12.0/24", "10.3.13.0/24"]
-  public_subnets  = ["10.3.1.0/24", "10.3.2.0/24", "10.3.3.0/24"]
+  azs             = local.azs
+  private_subnets = [for k, v in local.azs : cidrsubnet(local.config.networking.cidr, 8, k)]
+  public_subnets  = [for k, v in local.azs : cidrsubnet(local.config.networking.cidr, 8, k + 10)]
 
   tags = local.tags
 }
 
-resource "aws_route" "shrd_to_tgw_private" {
-  count                  = length(module.vpc_shrd.azs)
-  route_table_id         = module.vpc_shrd.private_route_table_ids[count.index]
+resource "aws_route" "networking_to_tgw_private" {
+  count                  = length(module.vpc_networking.azs)
+  route_table_id         = module.vpc_networking.private_route_table_ids[count.index]
   destination_cidr_block = "10.0.0.0/8"
   transit_gateway_id     = module.tgw.ec2_transit_gateway_id
 
   depends_on = [
-    aws_ec2_transit_gateway_vpc_attachment.tgw_attachment_shrd,
+    aws_ec2_transit_gateway_vpc_attachment.tgw_attachment_networking,
   ]
 }
 
-resource "aws_route" "shrd_to_tgw_public" {
-  route_table_id         = module.vpc_shrd.public_route_table_ids[0]
+resource "aws_route" "networking_to_tgw_public" {
+  route_table_id         = module.vpc_networking.public_route_table_ids[0]
   destination_cidr_block = "10.0.0.0/8"
   transit_gateway_id     = module.tgw.ec2_transit_gateway_id
 
   depends_on = [
-    aws_ec2_transit_gateway_vpc_attachment.tgw_attachment_shrd,
+    aws_ec2_transit_gateway_vpc_attachment.tgw_attachment_networking,
   ]
 }
 
-resource "aws_ec2_transit_gateway_vpc_attachment" "tgw_attachment_shrd" {
+resource "aws_ec2_transit_gateway_vpc_attachment" "tgw_attachment_networking" {
   transit_gateway_id = module.tgw.ec2_transit_gateway_id
 
   # Attach VPC and private subnets to the Transit Gateway
-  vpc_id     = module.vpc_shrd.vpc_id
-  subnet_ids = module.vpc_shrd.public_subnets
+  vpc_id     = module.vpc_networking.vpc_id
+  subnet_ids = module.vpc_networking.public_subnets
 
   # Turn off default route table association and propagation, as we're providing our own
   transit_gateway_default_route_table_association = false
   transit_gateway_default_route_table_propagation = false
 }
 
-resource "aws_ec2_transit_gateway_route_table" "tgw_rt_shrd" {
+resource "aws_ec2_transit_gateway_route_table" "tgw_rt_networking" {
   transit_gateway_id = module.tgw.ec2_transit_gateway_id
 }
 
-resource "aws_ec2_transit_gateway_route_table_association" "tgw_route_association_shrd" {
-  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.tgw_attachment_shrd.id
-  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw_rt_shrd.id
+resource "aws_ec2_transit_gateway_route_table_association" "tgw_route_association_networking" {
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.tgw_attachment_networking.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw_rt_networking.id
 }
 
-resource "aws_ec2_transit_gateway_route_table_propagation" "tgw_route_propagation_shrd_to_dev" {
+resource "aws_ec2_transit_gateway_route_table_propagation" "tgw_route_propagation_networking_to_dev" {
   transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.tgw_attachment_dev.id
-  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw_rt_shrd.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw_rt_networking.id
 }
 
-resource "aws_ec2_transit_gateway_route_table_propagation" "tgw_route_propagation_shrd_to_qa" {
+resource "aws_ec2_transit_gateway_route_table_propagation" "tgw_route_propagation_networking_to_qa" {
   transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.tgw_attachment_qa.id
-  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw_rt_shrd.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw_rt_networking.id
 }
 
 module "vpc_dev" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "~> 3.0"
 
-  name = "dev-vpc"
-  cidr = "10.1.0.0/16"
+  name = "${local.name}-dev"
+  cidr = local.config.dev.cidr
 
-  azs             = ["${local.region}a", "${local.region}b", "${local.region}c"]
-  private_subnets = ["10.1.11.0/24", "10.1.12.0/24", "10.1.13.0/24"]
-  public_subnets  = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24"]
+  azs             = local.azs
+  private_subnets = [for k, v in local.azs : cidrsubnet(local.config.dev.cidr, 8, k)]
+  public_subnets  = [for k, v in local.azs : cidrsubnet(local.config.dev.cidr, 8, k + 10)]
 
   create_igw = false
 
@@ -168,8 +183,8 @@ resource "aws_ec2_transit_gateway_route_table_association" "tgw_route_associatio
   transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw_rt_dev.id
 }
 
-resource "aws_ec2_transit_gateway_route_table_propagation" "tgw_route_propagation_dev_to_shrd" {
-  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.tgw_attachment_shrd.id
+resource "aws_ec2_transit_gateway_route_table_propagation" "tgw_route_propagation_dev_to_networking" {
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.tgw_attachment_networking.id
   transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw_rt_dev.id
 }
 
@@ -177,12 +192,12 @@ module "vpc_qa" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "~> 3.0"
 
-  name = "qa-vpc"
-  cidr = "10.2.0.0/16"
+  name = "${local.name}-qa"
+  cidr = local.config.qa.cidr
 
-  azs             = ["${local.region}a", "${local.region}b", "${local.region}c"]
-  private_subnets = ["10.2.11.0/24", "10.2.12.0/24", "10.2.13.0/24"]
-  public_subnets  = ["10.2.1.0/24", "10.2.2.0/24", "10.2.3.0/24"]
+  azs             = local.azs
+  private_subnets = [for k, v in local.azs : cidrsubnet(local.config.qa.cidr, 8, k)]
+  public_subnets  = [for k, v in local.azs : cidrsubnet(local.config.qa.cidr, 8, k + 10)]
 
   create_igw = false
 
@@ -231,8 +246,8 @@ resource "aws_ec2_transit_gateway_route_table_association" "tgw_route_associatio
   transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw_rt_qa.id
 }
 
-resource "aws_ec2_transit_gateway_route_table_propagation" "tgw_route_propagation_qa_to_shrd" {
-  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.tgw_attachment_shrd.id
+resource "aws_ec2_transit_gateway_route_table_propagation" "tgw_route_propagation_qa_to_networking" {
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.tgw_attachment_networking.id
   transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw_rt_qa.id
 }
 
@@ -241,7 +256,7 @@ resource "aws_ec2_transit_gateway_route_table_propagation" "tgw_route_propagatio
 ################################################################################
 
 resource "aws_key_pair" "deployer" {
-  key_name   = "deployer-key"
+  key_name   = "${local.name}-deployer"
   public_key = var.public_key
 }
 
@@ -255,13 +270,13 @@ data "aws_ami" "amazon_linux" {
   }
 }
 
-module "security_group_shrd" {
+module "security_group_networking" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "~> 4.0"
 
-  name        = "${local.name}-sg-shrd"
+  name        = "${local.name}-networking"
   description = "Security group for usage with EC2 instances"
-  vpc_id      = module.vpc_shrd.vpc_id
+  vpc_id      = module.vpc_networking.vpc_id
 
   ingress_cidr_blocks = ["0.0.0.0/0"]
   ingress_rules       = ["ssh-tcp"]
@@ -270,17 +285,17 @@ module "security_group_shrd" {
   tags = local.tags
 }
 
-module "ec2_shrd" {
+module "ec2_networking" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "~> 4.0.0"
 
-  name = "vm-shrd"
+  name = "${local.name}-networking"
 
   ami                         = data.aws_ami.amazon_linux.id
   instance_type               = "t2.micro"
-  availability_zone           = element(module.vpc_shrd.azs, 0)
-  subnet_id                   = element(module.vpc_shrd.public_subnets, 0)
-  vpc_security_group_ids      = [module.security_group_shrd.security_group_id]
+  availability_zone           = element(module.vpc_networking.azs, 0)
+  subnet_id                   = element(module.vpc_networking.public_subnets, 0)
+  vpc_security_group_ids      = [module.security_group_networking.security_group_id]
   associate_public_ip_address = true
 
   hibernation = true
@@ -310,7 +325,7 @@ module "security_group_dev" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "~> 4.0"
 
-  name        = "${local.name}-sg-dev"
+  name        = "${local.name}-dev"
   description = "Security group for usage with EC2 instances"
   vpc_id      = module.vpc_dev.vpc_id
 
@@ -325,7 +340,7 @@ module "ec2_dev" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "~> 4.0.0"
 
-  name = "vm-dev"
+  name = "${local.name}-dev"
 
   ami                         = data.aws_ami.amazon_linux.id
   instance_type               = "t2.micro"
@@ -361,7 +376,7 @@ module "security_group_qa" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "~> 4.0"
 
-  name        = "${local.name}-sg-qa"
+  name        = "${local.name}-qa"
   description = "Security group for usage with EC2 instances"
   vpc_id      = module.vpc_qa.vpc_id
 
@@ -376,7 +391,7 @@ module "ec2_qa" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "~> 4.0.0"
 
-  name = "vm-qa"
+  name = "${local.name}-qa"
 
   ami                         = data.aws_ami.amazon_linux.id
   instance_type               = "t2.micro"

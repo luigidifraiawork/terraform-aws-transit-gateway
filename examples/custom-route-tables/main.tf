1d43c7bbb7e9adf7d4da109bf5c1dbd3cda4711e
Luigi Di Fraia <luigidifraia+workrelated@gmail.com>
2022-05-13 14:59:14 +0100

    Use iterators for routes and spokes

--- examples/custom-route-tables/main.tf
+++ examples/custom-route-tables/main.tf
@@ -14,14 +14,21 @@ locals {
   config = {
     networking = {
       cidr = "10.1.0.0/16"
+      spoke = false
     }
     dev = {
       cidr = "10.2.0.0/16"
+      spoke = true
     }
     qa = {
       cidr = "10.3.0.0/16"
+      spoke = true
     }
   }
+
+  spokes = [ for i in keys(local.config) : i if local.config[i].spoke == true ]
+
+  tgw_routes = keys(local.config)
 
   tags = {
     Example    = local.name
@@ -59,27 +66,13 @@ module "tgw" {
   tags = local.tags
 }
 
-resource "aws_ec2_transit_gateway_route_table" "tgw_rt_networking" {
+resource "aws_ec2_transit_gateway_route_table" "tgw_route_tables" {
+  for_each = toset(local.tgw_routes)
+
   transit_gateway_id = module.tgw.ec2_transit_gateway_id
 
   tags = {
-    Name = "networking"
-  }
-}
-
-resource "aws_ec2_transit_gateway_route_table" "tgw_rt_dev" {
-  transit_gateway_id = module.tgw.ec2_transit_gateway_id
-
-  tags = {
-    Name = "dev"
-  }
-}
-
-resource "aws_ec2_transit_gateway_route_table" "tgw_rt_qa" {
-  transit_gateway_id = module.tgw.ec2_transit_gateway_id
-
-  tags = {
-    Name = "qa"
+    Name = "${each.key}"
   }
 }
 
@@ -87,35 +80,38 @@ resource "aws_ec2_transit_gateway_route_table" "tgw_rt_qa" {
 # Glue data sources to emulate multi-account resource lookups
 ################################################################################
 
-data "aws_ec2_transit_gateway_route_table" "tgw_rt_networking" {
+data "aws_ec2_transit_gateway_route_table" "tgw_route_tables" {
+  for_each = toset(local.tgw_routes)
+
   filter {
     name   = "tag:Name"
-    values = ["networking"]
+    values = ["${each.key}"]
   }
 
-  depends_on = [aws_ec2_transit_gateway_route_table.tgw_rt_networking]
-}
-
-data "aws_ec2_transit_gateway_route_table" "tgw_rt_dev" {
-  filter {
-    name   = "tag:Name"
-    values = ["dev"]
-  }
-
-  depends_on = [aws_ec2_transit_gateway_route_table.tgw_rt_dev]
-}
-
-data "aws_ec2_transit_gateway_route_table" "tgw_rt_qa" {
-  filter {
-    name   = "tag:Name"
-    values = ["qa"]
-  }
-
-  depends_on = [aws_ec2_transit_gateway_route_table.tgw_rt_qa]
+  depends_on = [aws_ec2_transit_gateway_route_table.tgw_route_tables]
 }
 
 ################################################################################
 # Supporting resources
+################################################################################
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "${local.name}-deployer"
+  public_key = var.public_key
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+################################################################################
+# Network Hub resources
 ################################################################################
 
 module "vpc_networking" {
@@ -167,161 +163,15 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "tgw_attachment_networking" {
 
 resource "aws_ec2_transit_gateway_route_table_association" "tgw_route_association_networking" {
   transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.tgw_attachment_networking.id
-  transit_gateway_route_table_id = data.aws_ec2_transit_gateway_route_table.tgw_rt_networking.id
-  #transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw_rt_networking.id
+  transit_gateway_route_table_id = data.aws_ec2_transit_gateway_route_table.tgw_route_tables["networking"].id
+  #transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw_rts["networking"].id
 }
 
-resource "aws_ec2_transit_gateway_route_table_propagation" "tgw_route_propagation_dev_to_networking" {
+resource "aws_ec2_transit_gateway_route_table_propagation" "tgw_route_propagation_spokes_to_networking" {
+  for_each = toset(local.spokes)
+
   transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.tgw_attachment_networking.id
-  transit_gateway_route_table_id = data.aws_ec2_transit_gateway_route_table.tgw_rt_dev.id
-  #transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw_rt_dev.id
-}
-
-resource "aws_ec2_transit_gateway_route_table_propagation" "tgw_route_propagation_qa_to_networking" {
-  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.tgw_attachment_networking.id
-  transit_gateway_route_table_id = data.aws_ec2_transit_gateway_route_table.tgw_rt_qa.id
-  #transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw_rt_qa.id
-}
-
-module "vpc_dev" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 3.0"
-
-  name = "${local.name}-dev"
-  cidr = local.config.dev.cidr
-
-  azs             = local.azs
-  private_subnets = [for k, v in local.azs : cidrsubnet(local.config.dev.cidr, 8, k)]
-  public_subnets  = [for k, v in local.azs : cidrsubnet(local.config.dev.cidr, 8, k + 10)]
-
-  create_igw = false
-
-  tags = local.tags
-}
-
-resource "aws_route" "dev_to_tgw_private" {
-  count                  = length(module.vpc_dev.azs)
-  route_table_id         = module.vpc_dev.private_route_table_ids[count.index]
-  destination_cidr_block = "0.0.0.0/0"
-  transit_gateway_id     = module.tgw.ec2_transit_gateway_id
-
-  depends_on = [
-    aws_ec2_transit_gateway_vpc_attachment.tgw_attachment_dev,
-  ]
-}
-
-resource "aws_route" "dev_to_tgw_public" {
-  route_table_id         = module.vpc_dev.public_route_table_ids[0]
-  destination_cidr_block = "0.0.0.0/0"
-  transit_gateway_id     = module.tgw.ec2_transit_gateway_id
-
-  depends_on = [
-    aws_ec2_transit_gateway_vpc_attachment.tgw_attachment_dev,
-  ]
-}
-
-resource "aws_ec2_transit_gateway_vpc_attachment" "tgw_attachment_dev" {
-  transit_gateway_id = module.tgw.ec2_transit_gateway_id
-
-  # Attach VPC and private subnets to the Transit Gateway
-  vpc_id     = module.vpc_dev.vpc_id
-  subnet_ids = module.vpc_dev.public_subnets
-
-  # Turn off default route table association and propagation, as we're providing our own
-  transit_gateway_default_route_table_association = false
-  transit_gateway_default_route_table_propagation = false
-}
-
-resource "aws_ec2_transit_gateway_route_table_association" "tgw_route_association_dev" {
-  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.tgw_attachment_dev.id
-  transit_gateway_route_table_id = data.aws_ec2_transit_gateway_route_table.tgw_rt_dev.id
-  #transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw_rt_dev.id
-}
-
-resource "aws_ec2_transit_gateway_route_table_propagation" "tgw_route_propagation_networking_to_dev" {
-  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.tgw_attachment_dev.id
-  transit_gateway_route_table_id = data.aws_ec2_transit_gateway_route_table.tgw_rt_networking.id
-  #transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw_rt_networking.id
-}
-
-module "vpc_qa" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 3.0"
-
-  name = "${local.name}-qa"
-  cidr = local.config.qa.cidr
-
-  azs             = local.azs
-  private_subnets = [for k, v in local.azs : cidrsubnet(local.config.qa.cidr, 8, k)]
-  public_subnets  = [for k, v in local.azs : cidrsubnet(local.config.qa.cidr, 8, k + 10)]
-
-  create_igw = false
-
-  tags = local.tags
-}
-
-resource "aws_route" "qa_to_tgw_private" {
-  count                  = length(module.vpc_qa.azs)
-  route_table_id         = module.vpc_qa.private_route_table_ids[count.index]
-  destination_cidr_block = "0.0.0.0/0"
-  transit_gateway_id     = module.tgw.ec2_transit_gateway_id
-
-  depends_on = [
-    aws_ec2_transit_gateway_vpc_attachment.tgw_attachment_qa,
-  ]
-}
-
-resource "aws_route" "qa_to_tgw_public" {
-  route_table_id         = module.vpc_qa.public_route_table_ids[0]
-  destination_cidr_block = "0.0.0.0/0"
-  transit_gateway_id     = module.tgw.ec2_transit_gateway_id
-
-  depends_on = [
-    aws_ec2_transit_gateway_vpc_attachment.tgw_attachment_qa,
-  ]
-}
-
-resource "aws_ec2_transit_gateway_vpc_attachment" "tgw_attachment_qa" {
-  transit_gateway_id = module.tgw.ec2_transit_gateway_id
-
-  # Attach VPC and private subnets to the Transit Gateway
-  vpc_id     = module.vpc_qa.vpc_id
-  subnet_ids = module.vpc_qa.public_subnets
-
-  # Turn off default route table association and propagation, as we're providing our own
-  transit_gateway_default_route_table_association = false
-  transit_gateway_default_route_table_propagation = false
-}
-
-resource "aws_ec2_transit_gateway_route_table_association" "tgw_route_association_qa" {
-  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.tgw_attachment_qa.id
-  transit_gateway_route_table_id = data.aws_ec2_transit_gateway_route_table.tgw_rt_qa.id
-  #transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw_rt_qa.id
-}
-
-resource "aws_ec2_transit_gateway_route_table_propagation" "tgw_route_propagation_networking_to_qa" {
-  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.tgw_attachment_qa.id
-  transit_gateway_route_table_id = data.aws_ec2_transit_gateway_route_table.tgw_rt_networking.id
-  #transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw_rt_networking.id
-}
-
-################################################################################
-# EC2 resources for testing connectivity
-################################################################################
-
-resource "aws_key_pair" "deployer" {
-  key_name   = "${local.name}-deployer"
-  public_key = var.public_key
-}
-
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-  owners      = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["amzn-ami-hvm-*-x86_64-gp2"]
-  }
+  transit_gateway_route_table_id = data.aws_ec2_transit_gateway_route_table.tgw_route_tables[each.key].id
 }
 
 module "security_group_networking" {
@@ -375,6 +225,71 @@ module "ec2_networking" {
   tags = local.tags
 }
 
+################################################################################
+# Dev Spoke resources
+################################################################################
+
+module "vpc_dev" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 3.0"
+
+  name = "${local.name}-dev"
+  cidr = local.config.dev.cidr
+
+  azs             = local.azs
+  private_subnets = [for k, v in local.azs : cidrsubnet(local.config.dev.cidr, 8, k)]
+  public_subnets  = [for k, v in local.azs : cidrsubnet(local.config.dev.cidr, 8, k + 10)]
+
+  create_igw = false
+
+  tags = local.tags
+}
+
+resource "aws_route" "dev_to_tgw_private" {
+  count                  = length(module.vpc_dev.azs)
+  route_table_id         = module.vpc_dev.private_route_table_ids[count.index]
+  destination_cidr_block = "0.0.0.0/0"
+  transit_gateway_id     = module.tgw.ec2_transit_gateway_id
+
+  depends_on = [
+    aws_ec2_transit_gateway_vpc_attachment.tgw_attachment_dev,
+  ]
+}
+
+resource "aws_route" "dev_to_tgw_public" {
+  route_table_id         = module.vpc_dev.public_route_table_ids[0]
+  destination_cidr_block = "0.0.0.0/0"
+  transit_gateway_id     = module.tgw.ec2_transit_gateway_id
+
+  depends_on = [
+    aws_ec2_transit_gateway_vpc_attachment.tgw_attachment_dev,
+  ]
+}
+
+resource "aws_ec2_transit_gateway_vpc_attachment" "tgw_attachment_dev" {
+  transit_gateway_id = module.tgw.ec2_transit_gateway_id
+
+  # Attach VPC and private subnets to the Transit Gateway
+  vpc_id     = module.vpc_dev.vpc_id
+  subnet_ids = module.vpc_dev.public_subnets
+
+  # Turn off default route table association and propagation, as we're providing our own
+  transit_gateway_default_route_table_association = false
+  transit_gateway_default_route_table_propagation = false
+}
+
+resource "aws_ec2_transit_gateway_route_table_association" "tgw_route_association_dev" {
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.tgw_attachment_dev.id
+  transit_gateway_route_table_id = data.aws_ec2_transit_gateway_route_table.tgw_route_tables["dev"].id
+  #transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw_rts["dev"].id
+}
+
+resource "aws_ec2_transit_gateway_route_table_propagation" "tgw_route_propagation_networking_to_dev" {
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.tgw_attachment_dev.id
+  transit_gateway_route_table_id = data.aws_ec2_transit_gateway_route_table.tgw_route_tables["networking"].id
+  #transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw_rts["networking"].id
+}
+
 module "security_group_dev" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "~> 4.0"
@@ -424,6 +339,71 @@ module "ec2_dev" {
   key_name = aws_key_pair.deployer.key_name
 
   tags = local.tags
+}
+
+################################################################################
+# QA Spoke resources
+################################################################################
+
+module "vpc_qa" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 3.0"
+
+  name = "${local.name}-qa"
+  cidr = local.config.qa.cidr
+
+  azs             = local.azs
+  private_subnets = [for k, v in local.azs : cidrsubnet(local.config.qa.cidr, 8, k)]
+  public_subnets  = [for k, v in local.azs : cidrsubnet(local.config.qa.cidr, 8, k + 10)]
+
+  create_igw = false
+
+  tags = local.tags
+}
+
+resource "aws_route" "qa_to_tgw_private" {
+  count                  = length(module.vpc_qa.azs)
+  route_table_id         = module.vpc_qa.private_route_table_ids[count.index]
+  destination_cidr_block = "0.0.0.0/0"
+  transit_gateway_id     = module.tgw.ec2_transit_gateway_id
+
+  depends_on = [
+    aws_ec2_transit_gateway_vpc_attachment.tgw_attachment_qa,
+  ]
+}
+
+resource "aws_route" "qa_to_tgw_public" {
+  route_table_id         = module.vpc_qa.public_route_table_ids[0]
+  destination_cidr_block = "0.0.0.0/0"
+  transit_gateway_id     = module.tgw.ec2_transit_gateway_id
+
+  depends_on = [
+    aws_ec2_transit_gateway_vpc_attachment.tgw_attachment_qa,
+  ]
+}
+
+resource "aws_ec2_transit_gateway_vpc_attachment" "tgw_attachment_qa" {
+  transit_gateway_id = module.tgw.ec2_transit_gateway_id
+
+  # Attach VPC and private subnets to the Transit Gateway
+  vpc_id     = module.vpc_qa.vpc_id
+  subnet_ids = module.vpc_qa.public_subnets
+
+  # Turn off default route table association and propagation, as we're providing our own
+  transit_gateway_default_route_table_association = false
+  transit_gateway_default_route_table_propagation = false
+}
+
+resource "aws_ec2_transit_gateway_route_table_association" "tgw_route_association_qa" {
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.tgw_attachment_qa.id
+  transit_gateway_route_table_id = data.aws_ec2_transit_gateway_route_table.tgw_route_tables["qa"].id
+  #transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw_rts["qa"].id
+}
+
+resource "aws_ec2_transit_gateway_route_table_propagation" "tgw_route_propagation_networking_to_qa" {
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.tgw_attachment_qa.id
+  transit_gateway_route_table_id = data.aws_ec2_transit_gateway_route_table.tgw_route_tables["networking"].id
+  #transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw_rts["networking"].id
 }
 
 module "security_group_qa" {
